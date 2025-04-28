28bf916f2b7c793b1403c43ff3ebaca973d570c5
Natália Santos <nataliassantosf@gmail.com>
2025-04-28 00:35:57 -0300

    chore: criacao de dependencias

---
+++ gymplanner/src/main/java/com/example/gymplanner/GymplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymplannerApplication.class, args);
+	}
+
+}
---
+++ gymplanner/src/test/java/com/example/gymplanner/GymplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
